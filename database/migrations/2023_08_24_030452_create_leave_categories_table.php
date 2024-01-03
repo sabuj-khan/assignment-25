3f547ae4e7e2d79a14a4b3aadad3b0afe3e0e19f
sabuj-khan <shobuj.khan1231@gmail.com>
2024-01-03 21:15:00 +0600

    Leave Management Application

--- database/migrations/2023_08_24_030452_create_leave_categories_table.php
+++ database/migrations/2023_08_24_030452_create_leave_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('leave_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
+            
 
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
